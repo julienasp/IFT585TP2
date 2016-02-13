ff90e8880c2bf0e50b2951b368d60f51a3b42e1d
julienasp <julien.aspirot@usherbrooke.ca>
2016-02-13 13:51:59 -0500

    maj

--- dataObject/arc.java
+++ dataObject/arc.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author JUASP-G73-Android
  */
-public class arc {
+package dataObject;
+public class Arc {
     
 }
--- dataObject/reseau.java
+++ dataObject/reseau.java
@@ -1,13 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author JUASP-G73-Android
  */
-public class reseau {
+package dataObject;
+
+import dataObject.routeur;
+import dataObject.hote;
+import dataObject.arc;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Hashtable;
+import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
+import protocole.UDPPacket;
+import server.UDPPacketHandler;
+import server.UDPServer;
+
+public class Reseau implements Runnable {
+    /**************************************/
+    /****** PUBLIC STATIC ATTRIBUTS *******/
+    /**************************************/
+    final static public int LSROUTING = 0;
+    final static public int DVROUTING = 1;
     
+    /**************************************/
+    /********* PRIVATE ATTRIBUTS **********/
+    /**************************************/
+    private static final Logger logger = Logger.getLogger(UDPServer.class);
+    private Hashtable<String, Routeur> listeRouteurs = new Hashtable<String, Routeur>();
+    private Hashtable<String, Arc> listeArcs = new Hashtable<String,Arc>();
+    private Hashtable<String, Hote> listeHotes = new Hashtable<String,Hote>();
+       
+    public void start() {		
+        logger.info("Reseau: Le reseau démarre.");
+        try {
+
+
+        } catch (Exception e) {
+                System.out.println("IO: " + e.getMessage());
+        }
+        finally {
+                logger.info("Fin du reseau");
+                stop();
+        }
+    }    
+        
+    public void stop() {		
+        try {
+                Thread.currentThread().interrupt();
+        } catch (Exception e) {
+                System.out.println("IO: " + e.getMessage());
+        }		
+    }
+    @Override
+    public void run() {
+            start();	
+    }
 }
